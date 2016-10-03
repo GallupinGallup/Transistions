89805d902a3e424d0edd10292c45940a7d68de9a
GallupinGallup <gallupingallup@gmail.com>
2016-10-03 12:02:22 -0600

    Created a new view and made a view controller to controller that.

---
+++ Transistions/Controller/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Transistions
+//
+//  Created by Gallup, Tyler on 10/3/16.
+//  Copyright © 2016 Gallup, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
