c7089b14efd2f9b5b963a95ef6c1ef23230fe453
GallupinGallup <gallupingallup@gmail.com>
2016-10-03 11:44:46 -0600

    Created a new project

---
+++ Transistions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transistions
+//
+//  Created by Gallup, Tyler on 10/3/16.
+//  Copyright © 2016 Gallup, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
